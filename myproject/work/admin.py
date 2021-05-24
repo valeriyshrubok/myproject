c3507f0e4e00f66a8b3386b3972aba0f9fcba5fb
valeriyshrubok <valerarabota5595@gmail.com>
2021-05-24 16:06:55 +0300

    Create admin

--- myproject/work/admin.py
+++ myproject/work/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, Chief, Paid_salary
+
+class EmloyeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'position', 'chief', 'salary', 'paid_salary')
+    list_display_links = ('id', 'chief')
+    search_fields = ('name', 'position')
+    list_filter = ('position', 'lvl')
+
+admin.site.register(Employee, EmloyeeAdmin)
+admin.site.register(Chief)
+admin.site.register(Paid_salary)
--- myproject/work/models.py
+++ myproject/work/models.py
@@ -2,16 +2,34 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=200)
-    position = models.CharField(max_length=200)
-    employment_date = models.DateTimeField('employment_date')
-    salary = models.IntegerField
-    lvl = models.CharField(max_length=50)
-    chief = models.ForeignKey('Chief', on_delete=models.PROTECT)
-    paid_salary = models.ForeignKey('Paid_salary', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, verbose_name='ФИО')
+    position = models.CharField(max_length=200, verbose_name='Должность')
+    employment_date = models.DateTimeField(verbose_name='Дата принятия на работу')
+    salary = models.IntegerField(default=0, verbose_name='ЗП')
+    lvl = models.CharField(max_length=50, verbose_name='Уровень')
+    chief = models.ForeignKey('Chief', on_delete=models.PROTECT, verbose_name='Начальник')
+    paid_salary = models.ForeignKey('Paid_salary', on_delete=models.CASCADE, verbose_name='Выплаченная зп')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудник'
 
 class Chief(models.Model):
     name = models.CharField(max_length=100, db_index=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Начальник'
+        verbose_name_plural = 'Начальники'
+
 class Paid_salary(models.Model):
-    info = models.IntegerField(db_index=True)
+    info = models.IntegerField(db_index=True, verbose_name='Выплаченная зп')
+
+    class Meta:
+        verbose_name = 'Выплаченная зарплата'
+        verbose_name_plural = 'Выплаченные зарплаты'
